480cb15409dece24fac4a98574a3f3542a4be86c
karenheyn <karenheyn@gmail.com>
2020-06-02 19:41:45 -0400

    added auth and users route

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express();
 
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
