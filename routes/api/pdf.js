c1162297a15666e337cbb0b4eef6b379c7f8c8cc
karenheyn <karenheyn@gmail.com>
2020-07-30 22:06:53 -0400

    added double sided routes

--- routes/api/pdf.js
+++ routes/api/pdf.js
@@ -47,5 +47,48 @@ router.get("/technicalboosterss", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+router.get("/basicdeckds", async (req, res) => {
+  try {
+    res.sendFile("single_sided/BBS_Basic_Deck_DS.pdf", { root: "pdf" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/cardbundleds", async (req, res) => {
+  try {
+    res.sendFile("single_sided/BBS_Card_Bundle_DS.pdf", { root: "pdf" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+router.get("/bundlewithboxds", async (req, res) => {
+  try {
+    res.sendFile("single_sided/BBS_Full_Bundle_with_Box_DS.pdf", {
+      root: "pdf",
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+router.get("/mysticalboosterds", async (req, res) => {
+  try {
+    res.sendFile("single_sided/BBS_Mystical_Booster_DS.pdf", { root: "pdf" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+router.get("/technicalboosterds", async (req, res) => {
+  try {
+    res.sendFile("single_sided/BBS_Technical_Booster_DS.pdf", { root: "pdf" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
