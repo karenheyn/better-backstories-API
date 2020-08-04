1611aa154740d302e6331fe4397f692f7aac7957
karenheyn <karenheyn@gmail.com>
2020-08-04 16:57:05 -0400

    chnged priceid

--- routes/api/stripe.js
+++ routes/api/stripe.js
@@ -10,16 +10,19 @@ router.post("/", async (req, res) => {
   console.log(req.body.product);
   switch (req.body.product.name) {
     case "Basic Deck":
-      amount = "price_1H0up7Kc91wTjOOikyrKImZs";
+      amount = "price_1HCUytK8COtkBsd0oegTrLha";
       break;
     case "Card Bundle":
-      amount = "price_1H9el6Kc91wTjOOiod2zfvtf";
+      amount = "price_1HCV2TK8COtkBsd0p9x0FsyM";
       break;
     case "Full Bundle with Box":
-      amount = "price_1H9empKc91wTjOOi15BO1q05";
+      amount = "price_1HCV3JK8COtkBsd0xBB3CMBk";
+      break;
+    case "Technical Booster":
+      amount = "price_1HCV4LK8COtkBsd01ArLHsCJ";
       break;
     default:
-      amount = "price_1H9enxKc91wTjOOi8Jjy0HnX";
+      amount = "price_1HCV3oK8COtkBsd0ahygDxWf";
   }
   const session = await stripe.checkout.sessions.create(
     {
